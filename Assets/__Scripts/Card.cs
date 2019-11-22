fdcb94c276b23892c6e4e8748dd71a2c753378b2
RichardNi1997 <richardni0827@gmail.com>
2019-11-21 22:16:32 -0500

    Up to UpdateDrawPile

--- Assets/__Scripts/Card.cs
+++ Assets/__Scripts/Card.cs
@@ -16,6 +16,8 @@ public class Card : MonoBehaviour {
 	public CardDefinition def;  // from DeckXML.xml		
 
 
+	public SpriteRenderer[] spriteRenderers;
+
 	public bool faceUp {
 		get {
 			return (!back.activeSelf);
@@ -26,12 +28,49 @@ public class Card : MonoBehaviour {
 		}
 	}
 
+	virtual public void OnMouseUpAsButton(){
+		print(name);
+	}
 
 	// Use this for initialization
 	void Start () {
-	
+		SetSortOrder(0);
 	}
 	
+	public void PopulateSpriteRenderers(){
+	
+		if(spriteRenderers == null || spriteRenderers.Length == 0){
+			spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+		}
+	}
+
+	public void SetSortingLayerName(string tSLN){
+		PopulateSpriteRenderers();
+
+		foreach (SpriteRenderer tSR in spriteRenderers){
+			tSR.sortingLayerName = tSLN;
+		}
+	}
+
+	public void SetSortOrder(int sOrd){
+		PopulateSpriteRenderers();
+
+		foreach(SpriteRenderer tSR in spriteRenderers){
+			if(tSR.gameObject == this.gameObject){
+				tSR.sortingOrder = sOrd;
+				continue;
+			}
+		switch (tSR.gameObject.name){
+			case "back":
+				tSR.sortingOrder = sOrd + 2;
+				break;
+			case "face":
+			default:
+				tSR.sortingOrder = sOrd + 1;
+				break;
+		}
+		}
+	}
 	// Update is called once per frame
 	void Update () {
 	
